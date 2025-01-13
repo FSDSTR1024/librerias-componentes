afb0ca86f2e69701b048294a6f1f9cc7bfbd6b88
rubenofen <rubenofen@gmail.com>
2025-01-13 18:04:51 +0000

    Project with css

---
+++ example-css/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Card } from "./components/Card";
+import { Card2 } from "./components/Card2";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Card />
+      <Card2 />
+    </>
+  );
+}
+
+export default App;
---
+++ example-css/src/components/Card2.jsx
@@ -0,0 +1,7 @@
+export const Card2 = () => {
+  return (
+    <div className="card">
+      <h1 className="card2-title">Card</h1>
+    </div>
+  );
+};
